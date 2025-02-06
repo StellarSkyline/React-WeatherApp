b018e82ad5cd86c61fefa6025f53c156b25fa571
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-02-05 20:21:20 -0600

    Update
    Search Api Works

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import './App.css'
 import SearchBar from './Components/SearchBar'
 import WeatherDetail from './Components/WeatherDetail'
 import { useState, useEffect } from 'react'
-import { getWeatherData } from './Services/ApiServices'
+import { getWeatherData, getSearchCitiesData } from './Services/ApiServices'
 
 function App() {
   const [weatherData, setWeatherData] = useState('')
+  const [searchCities, setSearchCities] = useState([])
 
   const getCurrentWeather = (city) => {
     getWeatherData(city).then((res)=> {
       setWeatherData(res.data)
+    })
+  }
+
+  const getSearchCities = (city) => {
+    getSearchCitiesData(city).then((res)=> {
+      setSearchCities(res.data)
     })
   }
 
--- src/Services/ApiServices.js
+++ src/Services/ApiServices.js
@@ -12,19 +12,7 @@ export async function getWeatherData(city) {
     return await apiRequest.get(`current.json?q=${city}`)
 }
 
-    // //API Call pattern using useEffect
-    // useEffect(() => {
-    //     const fetchJobs = async () => {
-    //         const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs'
-    //         try {
-    //             const res = await fetch(apiUrl)
-    //             const data = await res.json()
-    //             setJobs(data)
-    //         } catch (error) {
-    //             console.log("Error fetching Data", error)
-    //         } finally {
-    //             setLoading(false)
-    //         }
-    //     }
-    //     fetchJobs()
-    // }, [])
+export async function getSearchCitiesData(city) {
+    return await apiRequest.get(`search.json?q=${city}`)
+
+}
