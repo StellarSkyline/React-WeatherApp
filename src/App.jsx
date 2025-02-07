5d4c76f71d323846e0f24e80563cfc4399c42591
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-02-07 10:43:18 -0600

    Update
    Updated Logic to load in the right component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import './App.css'
 import SearchBar from './Components/SearchBar'
 import { useState } from 'react'
 import SearchList from './Components/SearchList'
+import WeatherDetail from './Components/WeatherDetail'
 
 
 function App() {
   const [showList, setShowList] = useState(true)
 
+  //Load in correct Components
+  const loadContent = (isList) => {
+    if(isList) {
+      return <SearchList showList={(res)=>{setShowList(res)}}/>
+    } else {
+      return <WeatherDetail/>
+    }
+
+  }
+
   return (
     <>
       <SearchBar />
-      <SearchList/>
+      {loadContent(showList)}
     </>
   )
 }
--- src/Components/SearchList.jsx
+++ src/Components/SearchList.jsx
@@ -4,14 +4,16 @@ import { getWeatherData } from '../Services/ApiServices'
 import { useDispatch } from 'react-redux'
 import { updateData } from '../Features/WeatherDataSlice'
 
-const SearchList = () => {
+const SearchList = ({showList}) => {
     const dispatch = useDispatch()
     //get City Search Data from redux store
     const cityData = useSelector(state=>state.city)
 
+    //Update current weather choice into redux store
     const onClick = (coordinates) => {
         getWeatherData(coordinates).then((res)=>{
             dispatch(updateData(res.data))
+            showList(false)
         })
     }
 
