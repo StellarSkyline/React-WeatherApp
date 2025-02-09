eb6a92d72004e86057fdeedfbb6fbd5eba6911ab
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-02-09 12:04:01 -0600

    Update
    Hide Api Key

--- src/Services/ApiServices.js
+++ src/Services/ApiServices.js
@@ -1,10 +1,11 @@
 import axios from "axios"
+import { ApiKey } from "./ApiKey"
 
 const apiRequest = axios.create({
     baseURL: 'http://api.weatherapi.com/v1/',
     headers: {
         'Content-Type': 'applicationn/json',
-        'key': '8c7556501dff4d0798b140838240912'
+        'key': ApiKey
     }
 })
 
